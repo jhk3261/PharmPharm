5f9a70834656cdd853655e8e8fddaebdd053f77a
jhk3261 <jhk3261@gnu.ac.kr>
2023-07-09 00:20:02 +0900

    구경하기로그인회원가입100퍼

--- static/js/list.js
+++ static/js/list.js
@@ -20,5 +20,23 @@ filters.forEach(filter => {
 
 })
 
+//돋보기 클릭 event
+const icon = document.querySelectorAll('.fa-magnifying-glass');
+const search = document.querySelectorAll('.Search');
+const input = document.querySelectorAll('.input');
+icon.forEach(function(item){
+    item.addEventListener("click", ()=>{
+        search.forEach(function(i){
+            i.classList.toggle('active');
+        })
+        icon.forEach(function(i){
+            i.classList.toggle('active');
+        })
+        input.forEach(function(i){
+            i.classList.toggle('active');
+        })
+    })
+})
+
 //글증가 페이지 번호 부여
 
